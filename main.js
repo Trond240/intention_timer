6783bb0e2214547a51472f264813476b66d35b0c
Alan Turing <trond@blacktie-llc.com>
2019-10-08 15:33:29 -0600

    added some java-script.

---
+++ main.js
@@ -0,0 +1,20 @@
+var studyButton = document.querySelector('.study-button');
+var meditateButton = document.querySelector('.meditate-button');
+var excerciseButton = document.querySelector('.excercise-button');
+var minutesInput = document.querySelector('.minutes');
+var secondsInput = document.querySelector('.seconds');
+var goalInput = document.querySelector('.goal');
+var startActivityBtn = document.querySelector('.start-activity');
+var asideContainer = document.querySelector('.activity-form');
+
+
+document.querySelector('.start-activity').addEventListener('click', showTimer);
+
+
+function showTimer() {
+  event.preventDefault();
+  asideContainer.innerHTml = `
+    <sectio class=''
+
+  `
+};
