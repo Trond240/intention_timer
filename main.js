e29fb11a5e5287ee0e656380b9a97eb04ede21f8
Alan Turing <trond@blacktie-llc.com>
2019-10-09 12:03:01 -0600

    Added functionality for inner HTML to switch to the timer page.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var minutesInput = document.querySelector('.minutes');
 var secondsInput = document.querySelector('.seconds');
 var goalInput = document.querySelector('.goal');
 var startActivityBtn = document.querySelector('.start-activity');
-var asideContainer = document.querySelector('.activity-form');
+var asideContainer = document.querySelector('.aside-form');
 
 
 document.querySelector('.start-activity').addEventListener('click', showTimer);
@@ -13,8 +13,16 @@ document.querySelector('.start-activity').addEventListener('click', showTimer);
 
 function showTimer() {
   event.preventDefault();
-  asideContainer.innerHTml = `
-    <sectio class=''
+  var timerTemplate = `
+<aside class='aside-timer'>
+    <section class='timer-page'>
+      <h2>${goalInput.value}</h2>
+      <h1 class='minutes-seconds'>${minutesInput.value}:${secondsInput.value}<h2>
+      <button class="Start">Start</button>
+    </section
+  </aside>
+  `;
 
-  `
+  var startTimer = document.querySelector('.start-activity');
+  asideContainer.innerHTML = timerTemplate;
 };
