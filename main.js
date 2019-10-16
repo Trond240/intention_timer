7b5023e45b4de410554329fe0d4c7d7d93b3a79c
caleb.haizlett@gmail.com <alanturing@example.c>
2019-10-16 15:23:23 -0600

    Clean js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-var minutesInput = document.querySelector('.minutes');
-var secondsInput = document.querySelector('.seconds');
-var goalInput = document.querySelector('.goal');
-var startActivityBtn = document.querySelector('.start-activity');
-var asideContainer = document.querySelector('.aside-form');
 var alertText = document.querySelector('.alert-text');
+var asideContainer = document.querySelector('.aside-form');
+var errorMessage = document.querySelector('.error-div');
+var exerciseButton = document.querySelector('.exercise-button');
 var formInput = document.querySelectorAll('input');
+var goalInput = document.querySelector('.goal');
 var inputGoal = document.getElementById('goal-input');
 var inputMinutes = document.getElementById('minutes-input');
 var inputSeconds = document.getElementById('seconds-input');
-var errorMessage = document.querySelector('.error-div');
-var timerBtn = document.querySelector('.start');
-var studyButton = document.querySelector('.study-button');
-var exerciseButton = document.querySelector('.exercise-button');
 var meditateButton = document.querySelector('.meditate-button');
+var minutesInput = document.querySelector('.minutes');
+var secondsInput = document.querySelector('.seconds');
+var startActivityBtn = document.querySelector('.start-activity');
+var studyButton = document.querySelector('.study-button');
+var timerBtn = document.querySelector('.start');
 
-// Function for input error
+// Checks if inputs are filled.
 startActivityBtn.addEventListener('click', startError);
 function startError() {
   if (inputGoal.value === '' &&
@@ -28,6 +28,7 @@ function startError() {
   }
 };
 
+// Displays error if an input is not filled.
 function errorImg() {
   document.querySelector('.error-span').classList.remove('hidden');
 };
@@ -50,66 +51,52 @@ inputSeconds.addEventListener('keyup', function(){
   }
 });
 
+// Enables button if inputs are filled.
 function startEnable() {
   startActivityBtn.disabled = false;
 };
 
-// var variable that changes depending on which button was picked
-// function categorySelect() {
-  // if blah was picked then add to variable
-  // put variable in function below to display which one was picked.
-// }
-
-// creater additonal properties in css.
+// Functions that toggle category button colors.
+var startColor = 0;
 studyButton.addEventListener('click', function(){
   event.preventDefault();
   studyButton.classList.toggle('active-study-button');
-  // timerBtn.classList.toggle('study-color');
-  // timerBtn.classList.remove('exercise-color');
-  // timerBtn.classList.remove('meditate-color');
   exerciseButton.classList.remove('active-exercise-button');
   meditateButton.classList.remove('active-meditate-button');
+  startColor = 1;
 });
 
 meditateButton.addEventListener('click', function(){
   event.preventDefault();
   meditateButton.classList.toggle('active-meditate-button');
-  // timerBtn.classList.toggle('meditate-color');
-  // timerBtn.classList.remove('study-color');
-  // timerBtn.classList.remove('exercise-color');
   studyButton.classList.remove('active-study-button');
   exerciseButton.classList.remove('active-exercise-button');
+  startColor = 2;
 });
 
 exerciseButton.addEventListener('click', function(){
   event.preventDefault();
   exerciseButton.classList.toggle('active-exercise-button');
-  // timerBtn.classList.toggle('exercise-color');
-  // timerBtn.classList.remove('study-color');
-  // timerBtn.classList.remove('meditate-color');
   studyButton.classList.remove('active-study-button');
   meditateButton.classList.remove('active-meditate-button');
+  startColor = 3;
 });
 
-
-// function for timer
-
-
-let countdown;
-
+// Sets up the seconds to be passed through the timer.
 function setTimer(event) {
   var sec = parseInt(inputSeconds.value);
   var min = parseInt(inputMinutes.value) * 60;
-  var time = (min + sec);
-  showTimer(time);
+  var seconds = (min + sec);
+  showTimer(seconds);
 }
 
+// Main function to display timer and cards.
 function showTimer(seconds) {
   const minutes1 = inputMinutes.value;
   var remainderSeconds = seconds % 60;
   var timerTemplate =
   `    <h1>Current Activity</h1>
-  <aside class='aside-timer'>
+    <aside class='aside-timer'>
     <section class='timer-page'>
         <h2 class='timer-heading'>${goalInput.value}</h2>
         <h2 class='minutes-seconds'>${minutes1}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}</h2>
@@ -121,13 +108,35 @@ function showTimer(seconds) {
     </aside>`;
   asideContainer.innerHTML = timerTemplate;
 
+// List of variables and event listeners used in display timer and cards.
+  let countdown;
+  var logButton = document.querySelector('.log-button');
+  var main = document.querySelector('.main');
   var startTimer = document.querySelector('.start');
+  var timerDisplay = document.querySelector('.minutes-seconds');
   startTimer.addEventListener('click', runTimer);
+  logButton.addEventListener('click', addCard);
 
+// Starts timer on click of start button.
   function runTimer(event) {
     counter(seconds);
+    selectCategory(startColor);
 }
 
+// Adds chosen categories color to the start button.
+  function selectCategory(startColor) {
+    if (startColor === 1) {
+      startTimer.classList.add('study-color');
+    }
+    if (startColor === 2) {
+      startTimer.classList.add('meditate-color');
+    }
+    if (startColor === 3) {
+      startTimer.classList.add('exercise-color');
+    }
+}
+
+// Timer countdown from time entered by user.
   function counter(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
@@ -142,25 +151,31 @@ function showTimer(seconds) {
       displayTimeLeft(secondsLeft);
     }, 1000);
   }
-  var main = document.querySelector('.main');
-  var timerDisplay = document.querySelector('.minutes-seconds');
-  var logButton = document.querySelector('.log-button');
 
+// Displays timer countdown and congratulatory messages.
   function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     var remainderSeconds = seconds % 60;
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
     timerDisplay.textContent = display;
-
     if (remainderSeconds === -0 && minutes === 0){
       startTimer.innerHTML = 'Task Complete!';
       timerDisplay.innerHTML = 'Good Job!';
       logButton.innerHTML += `<button type="button" class="log">log-activity</button>`;
-    };
+    }
 }
-  logButton.addEventListener('click', addCard);
+
+// Adds past activity cards with information based on user inputs.
   function addCard(){
+    if (startColor === 1) {
+      var category = 'Study';
+    } if (startColor === 2) {
+      var category = 'Meditate';
+    } if (startColor === 3) {
+      var category = 'Exercise';
+    }
     var mainTemplate = `<h2 class='main-title'>Past Activities</h2><div class='card'>
+     <h3>${category}</h3>
      <h5>${inputMinutes.value} Min ${inputSeconds.value} Seconds</h5>
      <h3>${goalInput.value}</h4>
      </div>`;
